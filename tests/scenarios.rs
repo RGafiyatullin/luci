1d64266f2885f2707c27f16dcaa0060588f6c91a
Raman Hafiyatulin <r.gafiyatullin@me.com>
2025-07-14 09:54:36 +0300

    chore: there will be snapshot-tests for standard scenarios

---
+++ tests/scenarios.rs
@@ -0,0 +1,19 @@
+use insta::{assert_debug_snapshot, assert_yaml_snapshot};
+use luci::{execution_graph::ExecutionGraph, messages::Messages, scenario::Scenario};
+use test_case::test_case;
+
+#[test_case("01")]
+fn run(name: &str) {
+    let input = format!("tests/scenarios/{name}.yaml");
+    let scenario: Scenario =
+        serde_yaml::from_str(std::fs::read_to_string(input).expect("read file").as_str())
+            .expect("serde::de");
+    assert_debug_snapshot!(format!("{name}.debug"), scenario);
+    assert_yaml_snapshot!(format!("{name}.yaml"), scenario);
+
+    let messages = Messages::new();
+
+    let _exec_graph = ExecutionGraph::builder(messages)
+        .build(&scenario)
+        .expect("build exec-graph");
+}
