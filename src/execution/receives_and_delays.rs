d16e2e9a89da5404ac2bfa43c5dfc22ebc6476a1
Raman Hafiyatulin <r.gafiyatullin@me.com>
2025-09-25 15:59:46 +0300

    fix: separate resolutions to wait before valid_from, and after

--- src/execution/receives_and_delays.rs
+++ src/execution/receives_and_delays.rs
@@ -9,7 +9,7 @@ const RECV_RESOLUTION_DIVISOR: u32 = 1000;
 
 #[derive(Default)]
 pub(crate) struct ReceivesAndDelays {
-    upcoming:   BTreeSet<UpcomingEntry>,
+    schedule:   BTreeSet<ScheduleEntry>,
     resolution: BTreeSet<ResolutionEntry>,
     valid_from: HashMap<KeyRecv, Instant>,
 }
@@ -21,53 +21,54 @@ pub(crate) enum KeyDelayOrRecv {
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-struct UpcomingEntry {
-    at:         Instant,
-    key:        KeyDelayOrRecv,
-    resolution: Duration,
+struct ScheduleEntry {
+    at:    Instant,
+    event: ScheduledEvent,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+enum ScheduledEvent {
+    Ripe(KeyDelayOrRecv),
+    UnsetResolution(ResolutionEntry),
+    SetResolution(ResolutionEntry),
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 struct ResolutionEntry {
     resolution: Duration,
     key:        KeyDelayOrRecv,
-    at:         Instant,
 }
 
 impl ReceivesAndDelays {
     pub(crate) fn next_sleep_until(&self, now: Instant) -> Option<Instant> {
-        assert_eq!(self.resolution.len(), self.upcoming.len());
-
         let ResolutionEntry { resolution, .. } = self.resolution.first().copied()?;
         let one_step_after_now = now.checked_add(resolution).unwrap_or(now);
 
-        let UpcomingEntry { at, .. } = self.upcoming.first().copied()?;
+        let ScheduleEntry { at, .. } = self.schedule.first().copied()?;
 
         Some(one_step_after_now.min(at))
     }
 
     pub(crate) fn select_ripe_keys(&mut self, now: Instant) -> Vec<KeyDelayOrRecv> {
-        assert_eq!(self.resolution.len(), self.upcoming.len());
-
         let mut out = vec![];
 
-        while self.upcoming.first().is_some_and(|ue| ue.at <= now) {
-            let UpcomingEntry {
-                at,
-                key,
-                resolution,
-            } = self
-                .upcoming
+        while self.schedule.first().is_some_and(|se| se.at <= now) {
+            let ScheduleEntry { event, .. } = self
+                .schedule
                 .pop_first()
                 .expect("wouldn't enter this loop otherwise");
-            let actually_removed = self.resolution.remove(&ResolutionEntry {
-                resolution,
-                key,
-                at,
-            });
-            assert!(actually_removed);
 
-            out.push(key);
+            match event {
+                ScheduledEvent::Ripe(key) => out.push(key),
+                ScheduledEvent::UnsetResolution(re) => {
+                    let actually_removed = self.resolution.remove(&re);
+                    assert!(actually_removed);
+                },
+                ScheduledEvent::SetResolution(re) => {
+                    let actually_inserted = self.resolution.insert(re);
+                    assert!(actually_inserted);
+                },
+            };
         }
 
         out
@@ -79,8 +80,15 @@ impl ReceivesAndDelays {
             .remove(&key)
             .expect("recv-key should have existed");
         let key = KeyDelayOrRecv::Recv(key);
-        self.upcoming.retain(|ue| ue.key != key);
+        self.schedule.retain(|ScheduleEntry { event, .. }| {
+            key != match event {
+                ScheduledEvent::Ripe(key) => *key,
+                ScheduledEvent::SetResolution(ResolutionEntry { key, .. }) => *key,
+                ScheduledEvent::UnsetResolution(ResolutionEntry { key, .. }) => *key,
+            }
+        });
         self.resolution.retain(|re| re.key != key);
+
         valid_from
     }
 
@@ -90,18 +98,18 @@ impl ReceivesAndDelays {
         let at = now.checked_add(delay_for).expect("please pretty please");
         let key = KeyDelayOrRecv::Delay(key);
 
-        let new_d_entry = self.upcoming.insert(UpcomingEntry {
+        let r_entry = ResolutionEntry { resolution, key };
+        let new_r_entry = self.resolution.insert(r_entry);
+        let new_s_entry_1 = self.schedule.insert(ScheduleEntry {
             at,
-            key,
-            resolution,
+            event: ScheduledEvent::UnsetResolution(r_entry),
         });
-        let new_s_entry = self.resolution.insert(ResolutionEntry {
-            resolution,
-            key,
+        let new_s_entry_2 = self.schedule.insert(ScheduleEntry {
             at,
+            event: ScheduledEvent::Ripe(key),
         });
 
-        assert!(new_d_entry && new_s_entry);
+        assert!(new_r_entry && new_s_entry_1 && new_s_entry_2);
     }
 
     pub(crate) fn insert_recv(&mut self, now: Instant, key: KeyRecv, event: &EventRecv) {
@@ -110,30 +118,45 @@ impl ReceivesAndDelays {
             .expect("exceeded the range of the Instant");
         self.valid_from.insert(key, valid_from);
 
+        let key = KeyDelayOrRecv::Recv(key);
+
+        // resolution for the period from `now` to `valid_from`
+        {
+            let resolution = valid_from.saturating_duration_since(now) / RECV_RESOLUTION_DIVISOR;
+            let r_entry = ResolutionEntry { key, resolution };
+            let new_r_entry = self.resolution.insert(r_entry);
+            let new_s_entry = self.schedule.insert(ScheduleEntry {
+                at:    valid_from,
+                event: ScheduledEvent::UnsetResolution(r_entry),
+            });
+
+            assert!(new_r_entry && new_s_entry);
+        }
+
         if let Some(timeout) = event.before_duration {
             let valid_thru = now.checked_add(timeout).expect("oh don't be ridiculous!");
 
-            let key = KeyDelayOrRecv::Recv(key);
             let resolution =
                 valid_thru.saturating_duration_since(valid_from) / RECV_RESOLUTION_DIVISOR;
 
-            let u_entry = UpcomingEntry {
-                at: valid_thru,
-                key,
-                resolution,
-            };
-            let r_entry = ResolutionEntry {
-                resolution,
-                key,
-                at: valid_thru,
-            };
+            let r_entry = ResolutionEntry { resolution, key };
 
-            let new_u_entry = self.upcoming.insert(u_entry);
-            assert!(new_u_entry);
+            let new_s_entry_1 = self.schedule.insert(ScheduleEntry {
+                at:    valid_thru,
+                event: ScheduledEvent::Ripe(key),
+            });
+            assert!(new_s_entry_1);
 
             if !resolution.is_zero() {
-                let new_r_entry = self.resolution.insert(r_entry);
-                assert!(new_r_entry);
+                let new_s_entry_2 = self.schedule.insert(ScheduleEntry {
+                    at:    valid_from,
+                    event: ScheduledEvent::SetResolution(r_entry),
+                });
+                let new_s_entry_3 = self.schedule.insert(ScheduleEntry {
+                    at:    valid_thru,
+                    event: ScheduledEvent::UnsetResolution(r_entry),
+                });
+                assert!(new_s_entry_2 && new_s_entry_3);
             }
         }
     }
